e5655222507c64833f0093f88629e7cc17603791
Daltro-Oliveira-Vinuto <daltroov777@gmail.com>
2025-12-16 07:03:44 -0300

    urls.py updated

--- projeto_django/urls.py
+++ projeto_django/urls.py
@@ -19,7 +19,6 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.contrib import admin
 from django.urls import path, include
 
-from debug_toolbar.toolbar import debug_toolbar_urls
 
 DEBUG = False
 
