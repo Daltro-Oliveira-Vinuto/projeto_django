46714c845de519004e645728bd2e274541698540
Daltro-Oliveira-Vinuto <daltroov777@gmail.com>
2025-12-16 04:35:50 -0300

    render.yaml web service updated"

--- build.sh
+++ build.sh
@@ -7,8 +7,8 @@ echo "installing dependencies with poetry"
 poetry install 
 
 
-echo "collecting static files"
-poetry run python manage.py collectstatic --no-input
+#echo "collecting static files"
+#poetry run python manage.py collectstatic --no-input
 
 echo "running migrations"
 poetry run python manage.py migrate
